b624de31d8cc64776db540baab0a6ce0fa05e2da
Zachary Coursey <zcoursey22@gmail.com>
2018-01-30 06:44:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
