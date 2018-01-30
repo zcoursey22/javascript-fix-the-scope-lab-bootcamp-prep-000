70937bfd81ece9b5eda83b29408596a14aacf17b
Zachary Coursey <zcoursey22@gmail.com>
2018-01-30 06:48:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n += 2
-
   // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
